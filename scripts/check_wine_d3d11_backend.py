90230e7b20144d73b8b59bcdc9cfc3bfccbd4e9e
Nilton Perim Neto <niltonperimneto@gmail.com>
2026-09-14 07:34:05 -0300

    d3d11: add standalone backend device allocation

--- scripts/check_wine_d3d11_backend.py
+++ scripts/check_wine_d3d11_backend.py
@@ -11,6 +11,7 @@ REQUIRED_HEADER = (
     "void (*flush)(struct d3d11_device_context *context);",
     "const struct d3d11_backend_ops *backend_ops;",
     "void *backend_private;",
+    "BOOL standalone_allocation;",
     "ID3D11On12Device1 ID3D11On12Device1_iface;",
     "HRESULT (*create_wrapped_resource)(struct d3d_device *device,",
     "void (*release_wrapped_resources)(struct d3d_device *device,",
@@ -30,6 +31,9 @@ REQUIRED_DEVICE = (
     "return IUnknown_QueryInterface(device->outer_unk, iid, out);",
     "*out = &device->ID3D11On12Device1_iface;",
     "device->ID3D11On12Device1_iface.lpVtbl = &d3d11_on12_device_vtbl;",
+    "struct d3d_device *d3d_device_create_backend(",
+    "d3d_device_init(device, &device->IUnknown_inner);",
+    "if (device->standalone_allocation)",
 )
 
 
